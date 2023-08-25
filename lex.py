0e94f46fcd04817c698894c52a8a1e97aa6d2520
chasserb <chasserb@childandfamilyagency.org>
2023-08-25 10:27:28 -0400

    Initial commit

---
+++ lex.py
@@ -0,0 +1,105 @@
+import sys, enum
+
+'''
+Determine where each token starts/stops
+WHILE(keyword) nums(identifier) >(operator) 0(num) REPEAT(keyword)
+'''
+class Lexer:
+    def __init__(self, source):
+        self.source = source + '\n' # Push source code to lexer as a string. Append a new line.
+        self.currChar = '' # Current char in string
+        self.currPos = -1 # Current pos in string
+        self.nextChar()
+    
+    # Process next char
+    def nextChar(self):
+        self.currPos += 1
+        if self.currPos >= len(self.source):
+            self.currChar = '\0'  
+        else:
+            self.currChar = self.source[self.currPos]
+    
+    # Return lookahead char
+    def peek(self):
+        if self.currPos + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.currPos + 1]
+
+    # Invalid token, error out
+    def abort(self, source):
+        sys.exit("Lex error " + source)
+    
+    
+    # Skip whitespace, not new lines
+    def skip_whitespace(self):
+        pass
+
+    # Skip comments
+    def skip_comments(self):
+        pass
+
+    # Return next token
+    def getToken(self):
+        pass
+
+    def getToken(self):
+        token_map = {
+            '+': TokenType.PLUS,
+            '-': TokenType.MINUS,
+            '*': TokenType.ASTERISK,
+            '/': TokenType.SLASH,
+            r'\n': TokenType.NEWLINE,
+            r'\0': TokenType.EOF,
+        }
+
+        '''
+        If the current char is in the token_map(dict), take the current char, the token_map and assign '[]' self.currChar.
+        '''
+        if self.currChar in token_map:
+            token = Token(self.currChar, token_map[self.currChar])
+        else:
+            sys.exit("Error" + self.currChar)
+        # Go onto the next cahr
+        self.nextChar()
+        return token
+
+
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText # actual text 'PLUS'
+        self.kind = tokenKind # The token assigned '+'
+
+
+
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    
+    # Keywords
+    label = 101
+    goto = 102
+    print = 103
+    input = 104
+    let = 105
+    IF = 106
+    then = 107
+    endif = 108
+    WHILE = 109
+    repeat = 110
+    endwhile = 111
+
+    # Operators
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
